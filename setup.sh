7995350219ed330cdb2b83a7c23864f616f07f24
sunowsir <sunow.wang@gmail.com>
2019-11-24 21:21:40 +0800

    update : setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install gcc g++ python3 
+sudo apt-get install wget curl gcc g++ python3 python3-pip
 
 cd ~/
 
@@ -11,3 +11,6 @@ mv -f ~/.vimrc ~/.vimrc_old
 git clone git@github.com:sunowsir/.vim.git
 
 vim -c "PlugInstall" -c "q" -c "q"
+
+./plugged/YouCompleteMe/install.sh
+

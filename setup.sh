bdd14c1b8fe987e19417b30f5bb0eb2a85790f00
sunowsir <sunow.wang@gmail.com>
2019-12-25 22:42:07 +0800

    update : setup.sh

--- setup.sh
+++ setup.sh
@@ -40,12 +40,6 @@ git clone https://github.com/sunowsir/.vim.git
 
 judgeErrorExit ${?} "git clone https://github.com/sunowsir/.vim.git"
 
-mkdir ~/.vim/plugged
-cd ~/.vim/plugged
-git clone https://gitee.com/mirrors/youcompleteme.git
-mv ./youcompleteme YouCompleteMe
-cd ~/
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 judgeErrorExit ${?} "download `plug.vim` failed."

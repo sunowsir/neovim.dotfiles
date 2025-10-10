de7dd7c19ec056a5fc6825e88592d6bd5303172f
sun.wang <sun@ASUS-DESKTOP.localdomain>
2025-10-10 21:06:01 +0800

    [clean] clean lsp warning

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,14 +6,14 @@ return {
 
     -- opts 配置
     opts = {
-        bigfile = { enabled = true },
-        dashboard = { enabled = true },
-        explorer = { enabled = true },
-        indent = { enabled = false },
-        input = { enabled = true },
-        notifier = { enabled = true },
-        picker = { prompt = "  " },
-        quickfile = { enabled = true },
+        bigfile      = { enabled = true },
+        dashboard    = { enabled = true },
+        explorer     = { enabled = true },
+        indent       = { enabled = false },
+        input        = { enabled = true },
+        notifier     = { enabled = true },
+        picker       = { prompt  = "  " },
+        quickfile    = { enabled = true },
         statuscolumn = { enabled = true },
     },
 
@@ -21,33 +21,39 @@ return {
     keys = {
         {
             "<leader>fb",
-            function() Snacks.picker.buffers() end,
+            function() require("snacks").picker.buffers() end,
             desc = "Buffers"
         },
         {
             "<leader>ff",
-            function() Snacks.picker.files() end,
+            function() require("snacks").picker.files() end,
             desc = "Find Files"
         },
         {
             "<leader>fg",
-            function() Snacks.picker.grep() end,
+            function() require("snacks").picker.grep() end,
             desc = "Grep"
         },
         {
             "<leader>fe",
-            function() Snacks.explorer() end,
+            function() require("snacks").explorer() end,
             desc = "File Explorer"
         }
     },
 
     config = function()
+        local set_bg_callback = function()
+            vim.api.nvim_set_hl(0, "SnacksPicker",
+                { bg = "none", nocombine = true })
+
+            vim.api.nvim_set_hl(0, "SnacksPickerBorder",
+                { fg = "#316c71", bg = "none", nocombine = true })
+        end
+
+        -- set gb is none
         vim.api.nvim_create_autocmd("ColorScheme", {
             pattern = "*",
-            callback = function()
-                vim.api.nvim_set_hl(0, "SnacksPicker", { bg = "none", nocombine = true })
-                vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#316c71", bg = "none", nocombine = true })
-            end,
+            callback = set_bg_callback
         })
     end
 }
